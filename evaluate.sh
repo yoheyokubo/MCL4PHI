ec99c65f4fbc714ec2129709cf792a458c6c4ed3
YoheiOkubo <45516688+yoheyokubo@users.noreply.github.com>
2024-11-01 22:30:50 +0900

    Update evaluate.sh

--- evaluate.sh
+++ evaluate.sh
@@ -5,6 +5,6 @@ data_dir="data_cherry"
 data="cherry"
 
 model='cl4phi'
-model_pretrained='out_cherry_v3/model-cl4phi-data-cherry-granu-species-epoch-150-aug-0.0:0.5-seed-123.pth'
+model_pretrained='out_cherry/model-cl4phi-data-cherry-granu-species-epoch-150-aug-0.0_0.5_0.9_0.99_0.999-seed-123.pth'
 
 python3 $CODE --model $model --data $data --model_pretrained $model_pretrained --out_dir $out_dir --data_dir $data_dir
